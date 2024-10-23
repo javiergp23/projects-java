cc8f1de63b36e9c83fd3b46eea352cea40caf193
Javier Gonzalez <javgon2306@gmail.com>
2024-10-22 23:11:27 -0300

    fix contraseñas

--- EjeciciosBasicos/src/GeneradorDeContraseñas.java
+++ EjeciciosBasicos/src/GeneradorDeContraseñas.java
@@ -1,14 +1,21 @@
 public class GeneradorDeContraseñas {
     public static void main(String[] args) {
-        String caracteres ="0123456789";
+        String caracteres ="abcdefghi0123456789";
         String password = "";
         int cantindadCaracteres = 10;
+        int contador = 0;
+        while(contador < cantindadCaracteres){
+            contador++;
+            double aleatorio = Math.random();
+            aleatorio = Math.floor(aleatorio * 10);
+            int numero = (int) aleatorio;
 
-        double aleatorio = Math.random();
-        aleatorio = Math.floor(aleatorio * 10);
-        int numero = (int) aleatorio;
-        password += aleatorio;
-        System.out.println(aleatorio);
+            password += caracteres.charAt(numero);
+
+            //password += numero;
+            //System.out.println(numero);
+        }
+        System.out.println(password);
 
     }
 }

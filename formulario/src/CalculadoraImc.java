e70ec44c8087bfe582c5ae95d2350678cf3cc81c
Javier Gonzalez <javgon2306@gmail.com>
2024-10-24 10:03:47 -0300

    ádd jankenpon

---
+++ formulario/src/CalculadoraImc.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class CalculadoraImc {
+    private JButton button1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JPanel mainPanel;
+}
---
+++ jankenpon/src/Jankenpon.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Jankenpon {
+    public void jugar(){
+        System.out.println("Juguemos Piedra, Papel o tijeras");
+        System.out.println("Escribe alguno de estos:  Piedra, Papel, o tijeras");
+
+        Scanner teclado = new Scanner(System.in);
+        String respuestaDelUsuario = teclado.nextLine();
+
+        String respuestaDeLaComputadora = "";
+
+        Random generarNumerosAleatorios = new Random();
+        int numAleatorio = generarNumerosAleatorios.nextInt(3);
+        if(numAleatorio == 0){
+            respuestaDeLaComputadora = "Piedra";
+        }
+        if(numAleatorio == 1){
+            respuestaDeLaComputadora = "Papel";
+        }
+        if(numAleatorio == 2){
+            respuestaDeLaComputadora = "Tijera";
+        }
+        System.out.println("La computadora escogió: " + respuestaDeLaComputadora);
+
+        //Empate
+        if(respuestaDelUsuario.equals(respuestaDeLaComputadora)){
+            System.out.println("EMPATE!");
+        }else if(respuestaDelUsuario.equals("Piedra") && respuestaDeLaComputadora.equals("Tijera")){
+            System.out.println("Ganaste");
+        }else if(respuestaDelUsuario.equals("Tijera") && respuestaDeLaComputadora.equals("Papel")){
+            System.out.println("Ganaste");
+        }else if(respuestaDelUsuario.equals("Papel") && respuestaDeLaComputadora.equals("Piedra")){
+            System.out.println("Ganaste");
+        }else {
+            System.out.println("PERDISTE!");
+        }
+
+
+    }
+
+}
